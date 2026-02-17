c0b3724f1a1c2a4219e4980b714b1742c1be9c87
Romi García <miagar11@outlook.es>
2026-02-16 22:55:54 -0600

    Cambio de idomas atravez de polimorfismo

--- src/actividad4/ui/Language.java
+++ src/actividad4/ui/Language.java
@@ -1,48 +1,23 @@
 package actividad4.ui;
+/**
+ * Clase abstracta que define las cadenas de texto utilizadas en la interfaz del juego,
+ * permitiendo la internacionalización (soporte para diferentes idiomas).
+ */
+public abstract class Language {
+    public  String JUGADOR1;
+    public  String JUGADOR2;
+    public String GANADOR;
+    public String EMPATE;
+    public String J1;
+    public String J2;
+    public String CPU;
+    public String MODE_TITLE;
+    public String MODE_QUESTION;
+    public String  MODE_1P;
+    public String  MODE_2P;
+    public String  LANG_TITLE;
+    public String LANG_QUESTION;
+    public String REINICIAR;
+    public String PREGUNTA_REINICIO;
 
-public class Language {
-
-    private static String idioma = "ES";
-    public static void setIdioma(String codigo) {
-        idioma = codigo;
-    }
-
-    public static String get(String clave) {
-        if (idioma.equals("EN")) {
-// ingle'
-            switch (clave) {
-                case "TITLE": return "Tic Tac Toe";
-                case "TURN": return "Turn: ";
-                case "WINNER": return "Winner: ";
-                case "DRAW": return "Draw!";
-                case "PLAYER_1": return "Player 1";
-                case "PLAYER_2": return "Player 2";
-                case "CPU": return "Computer";
-                case "MODE_TITLE": return "Select Mode";
-                case "MODE_QUESTION": return "How do you want to play?";
-                case "MODE_1P": return "1 Player (vs CPU)";
-                case "MODE_2P": return "2 Players";
-                case "LANG_TITLE": return "Language";
-                case "LANG_QUESTION": return "Select a language / Selecciona idioma";
-            }
-        } else {
-//epañol'
-            switch (clave) {
-                case "TITLE": return "Gato / Tres en Raya";
-                case "TURN": return "Turno de: ";
-                case "WINNER": return "Ganador: ";
-                case "DRAW": return "¡Empate!";
-                case "PLAYER_1": return "Jugador 1";
-                case "PLAYER_2": return "Jugador 2";
-                case "CPU": return "CPU";
-                case "MODE_TITLE": return "Modo de Juego";
-                case "MODE_QUESTION": return "¿Cómo quieres jugar?";
-                case "MODE_1P": return "1 Jugador (vs CPU)";
-                case "MODE_2P": return "2 Jugadores";
-                case "LANG_TITLE": return "Idioma";
-                case "LANG_QUESTION": return "Selecciona idioma / Select a language";
-            }
-        }
-        return clave;
-    }
 }
